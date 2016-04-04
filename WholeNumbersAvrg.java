e6dc950a13465e12b0bc9fec5631c4ff8c3600dd
Jonathan Margulies <jam246960@email.vccs.edu>
2016-04-04 14:46:03 -0400

    Add WholeNumbersAvrg.java

---
+++ WholeNumbersAvrg.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class WholeNumbersAvrg {
+	public static void main(String [] args) {
+		Scanner Scan = new Scanner(System.in);
+		System.out.println("Please enter one whole number.");
+		int WholeNum1 = Scan.nextInt();
+		System.out.println("Please enter another whole number.");
+		int WholeNum2 = Scan.nextInt();
+		System.out.println("Please enter one more whole number.");
+		int WholeNum3 = Scan.nextInt();
+		int SumOfNum = WholeNum1 + WholeNum2 + WholeNum3;
+		int AverageOfNum = SumOfNum/3;
+		System.out.println("The average of these three numbers is " + AverageOfNum + ".");
+		
+	}
+}
